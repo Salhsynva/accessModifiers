9d732abafeae480cfc695c92840790c64a8d87dd
Salhsynva <100703384+Salhsynva@users.noreply.github.com>
2022-03-28 22:31:36 +0400

    Add files via upload

---
+++ accessModifiers/accessModifiers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace accessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("istifadeci adinizi daxil edin: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("parolunuzu daxil edin: ");
+            string password = Console.ReadLine();
+
+            User user = new User(username);
+            user.Password = password;
+
+            Console.WriteLine($"istifadeci adi: {user.Username} - parol: {user.Password}");
+        }
+    }
+}
---
+++ accessModifiers/accessModifiers/User.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace accessModifiers
+{
+    class User
+    {
+        public User(string username)
+        {
+            this.Username = username;
+        }
+        private string _username;
+        private string _password;
+
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value) && value.Length <= 25 && value.Length > 5)
+                    _username = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value) && value.Length <= 25 && value.Length > 7)
+                {
+                    if (HasDigit(value) && HasLower(value) && HasUpper(value) && !HasSpace(value))
+                        _password = value;
+                }
+            }
+        }
+
+        public bool HasDigit(string str)
+        {
+            foreach (var item in str)
+            {
+                if (char.IsDigit(item))
+                    return true;
+            }
+            return false;
+        }
+        public bool HasUpper(string str)
+        {
+            foreach (var item in str)
+            {
+                if (char.IsUpper(item))
+                    return true;
+            }
+            return false;
+        }
+        public bool HasLower(string str)
+        {
+            foreach (var item in str)
+            {
+                if (char.IsLower(item))
+                    return true;
+            }
+            return false;
+        }
+        public bool HasSpace(string str)
+        {
+            foreach (var item in str)
+            {
+                if (item == ' ')
+                    return true;
+            }
+            return false;
+        }
+    }
+}
